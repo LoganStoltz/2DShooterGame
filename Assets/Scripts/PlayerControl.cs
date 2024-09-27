ab39623385e2b3205ed634bd8487be00a60c9629
Logan Stoltz <102991130+LoganStoltz@users.noreply.github.com>
2024-09-26 17:42:02 -0700

    Added Basic Movement and Shooting

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+
+    // Gun Vars
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private Transform firingPoint;
+    [Range(0.1f, 1f)]
+    [SerializeField] private float fireRate = 0.5f;
+
+    private float fireTimer;
+
+
+    // Player Vars
+    public float movSpeed;
+    float speedX, speedY;
+    Rigidbody2D rb;
+
+    private Vector2 mousePos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        speedX = Input.GetAxisRaw("Horizontal") * movSpeed; 
+        speedY = Input.GetAxisRaw("Vertical") * movSpeed;
+        rb.velocity = new Vector2(speedX, speedY);
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        float angle = Mathf.Atan2(mousePos.y - transform.position.y, mousePos.x - transform.position.x) * Mathf.Rad2Deg - 90f;
+
+        transform.localRotation = Quaternion.Euler(0,0, angle); 
+
+        if(Input.GetMouseButton(0) && fireTimer <= 0f) {
+            Shoot();
+            fireTimer = fireRate;
+        }
+        else {
+            fireTimer -= Time.deltaTime;
+        }
+    }
+
+    private void Shoot()
+    {
+        Instantiate(bulletPrefab, firingPoint.position, firingPoint.rotation);
+    }
+}
