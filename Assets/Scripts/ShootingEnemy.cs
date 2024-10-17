59709af1321454b2c3d072bccf7b1afb159f1119
Logan Stoltz <102991130+LoganStoltz@users.noreply.github.com>
2024-10-16 23:13:18 -0700

    trying to add an enemy that can detect the player and shoot at them

---
+++ Assets/Scripts/ShootingEnemy.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingEnemy : MonoBehaviour
+{
+    [SerializeField] private float damage;
+    public GameObject enemy;
+    public Transform target;
+    public float speed = 2f;
+    public Vector2 distanceFromTarget;
+
+    [Range(0.1f, 1f)]
+    private float fireRate = 0.1f;
+    private float fireTimer;
+    [SerializeField] private Transform firingPoint;
+    
+
+    public float rotateSpeed = 0.0025f;
+    private Rigidbody2D rb;
+
+    public GameObject bulletPrefab; // Ammo
+    public GameObject dropItemPrefab; // The item to be dropped
+    public float dropChance = 0.8f; // 80% chance
+
+    private void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update() {
+        // Get the target
+        if (!target) {
+            GetTarget();
+
+            if(fireTimer <= 0){
+                ShootTarget();
+                fireTimer = fireRate;
+            }
+        }
+        // Rotate towards the target
+        else {
+            RotateTowardsTarget();
+            fireTimer -= Time.deltaTime;
+        }
+        distanceFromTarget = (target.position - enemy.transform.position);
+    }
+
+    private void FixedUpdate() 
+    {
+        // Move Forwards
+        rb.velocity = transform.up * speed;
+    }
+
+    private void RotateTowardsTarget() {
+        Vector2 targetDirection = target.position - transform.position;
+        float angle = Mathf.Atan2(targetDirection.y, targetDirection.x) * Mathf.Rad2Deg - 90f;
+        Quaternion q = Quaternion.Euler(new Vector3(0, 0, angle));
+        transform.localRotation = Quaternion.Slerp(transform.localRotation, q, rotateSpeed); // Will take time to rotate
+    }
+
+
+    private void GetTarget() 
+    {
+        if(GameObject.FindGameObjectWithTag("Player"))
+            target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.GetComponent<PlayerStats>().TakeDamage(damage);
+        }
+        else if(other.gameObject.CompareTag("Bullet"))
+        {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+            DropItem();
+            GameController.manager.IncreaseScore(1);
+        }
+    }
+
+    private void DropItem()
+    {
+        float randomValue = Random.value;
+
+        if (randomValue <= dropChance)
+        {
+            Instantiate(dropItemPrefab, transform.position, Quaternion.identity);
+            Debug.Log("item dropped");
+        }
+    }
+
+    private void ShootTarget()
+    {
+        if(distanceFromTarget.x < 10f && distanceFromTarget.y < 10000f)
+            Instantiate(bulletPrefab, firingPoint.position, firingPoint.rotation);
+    }
+}
